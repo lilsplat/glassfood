c00355a66ebdd4e5c04987eb40a9ca2d3ac307f0
Lily Xie <lxie@wellesley.edu>
2014-12-07 14:12:16 -0500

    Final project

--- Examples_SDK/CustomRenderer/gen/com/metaio/example_custom_renderer/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.metaio.example_custom_renderer;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- Template/gen/com/hci/glassrafael/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.hci.glassrafael;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
